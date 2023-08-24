e91a47743789c0d06efff183beaa0e520e9cb63c
riyazbaghdad <riyazdn@gmail.com>
2023-08-24 14:04:15 +0200

    Initial commit - created an initial Xcode Project

---
+++ cpp-template/cpp-template/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cpp-template
+//
+//  Created by Riyaz Baghdad on 24.08.23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
